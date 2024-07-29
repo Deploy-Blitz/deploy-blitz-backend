6d46419516006d379a50de3075b316f05ab44ca2
Luigi Vismara <33238929+luigivis@users.noreply.github.com>
2024-07-28 21:30:24 -0600

    feat: add git clone, get script as string, validate existence of script

--- src/main/java/com/deployblitz/backend/config/SwaggerConfig.java
+++ src/main/java/com/deployblitz/backend/config/SwaggerConfig.java
@@ -5,12 +5,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.media.Content;
-import io.swagger.v3.oas.models.media.MediaType;
 import io.swagger.v3.oas.models.media.Schema;
-import io.swagger.v3.oas.models.responses.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,16 +14,11 @@ import org.springframework.context.annotation.Configuration;
 import java.util.HashMap;
 import java.util.Map;
 
-import static java.lang.StringTemplate.STR;
-
 @Slf4j
 @Configuration
 public class SwaggerConfig {
     @Value("${project.version}")
     private String version;
-
-    @Value("${server.port}")
-    private String port;
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -53,21 +44,6 @@ public class SwaggerConfig {
 
         return new OpenAPI().info(info);
     }
-
-//    @Bean
-//    public OpenApiCustomizer genericResponseCustomizer() {
-//        return openApi -> {
-//            openApi.getPaths().forEach((path, pathItem) -> {
-//                log.info("Path: {}", path);
-//                pathItem.readOperations().forEach(operation -> {
-//                    operation.getResponses().addApiResponse("200", new ApiResponse()
-//                            .description("OK")
-//                            .content(new Content().addMediaType("application/json",
-//                                    new MediaType().schema(genericResponseSchema()))));
-//                });
-//            });
-//        };
-//    }
 
     private static Map<String, Schema> schemas() {
         var schemas = new HashMap<String, Schema>();
